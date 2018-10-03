d01e57b5c66fc15f3e6da97700010067356e585d
saumya <saumyawardhan1@gmail.com>
2018-10-04 03:52:13 +0530

    Add Icons and Flexbox to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,17 @@ class Navbar extends Component {
     render() {
         return (
             <div className="topnav">
-                <a href="https://join.slack.com/t/firstcontributors/shared_invite/enQtMzE1MTYwNzI3ODQ0LTZiMDA2OGI2NTYyNjM1MTFiNTc4YTRhZTg4OWZjMzA0ZWZmY2UxYzVkMzI1ZmVmOWI4ODdkZWQwNTM2NDVmNjY"  target="_blank" rel="noopener noreferrer">Slack</a>
-                <a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer">Twitter</a>
-                <a href="https://github.com/Roshanjossey/first-contributions" target="_blank" rel="noopener noreferrer">GitHub</a>
+            	<ul>
+            		<li>
+            			<a href="https://join.slack.com/t/firstcontributors/shared_invite/enQtMzE1MTYwNzI3ODQ0LTZiMDA2OGI2NTYyNjM1MTFiNTc4YTRhZTg4OWZjMzA0ZWZmY2UxYzVkMzI1ZmVmOWI4ODdkZWQwNTM2NDVmNjY"  target="_blank" rel="noopener noreferrer"><i className="fa fa-slack"></i> Slack</a>
+            		</li>
+            		<li>
+            			<a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i> Twitter</a>
+            		</li>
+            		<li>
+            			<a href="https://github.com/Roshanjossey/first-contributions" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i> GitHub</a>
+            		</li>
+            	</ul>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <App />,
