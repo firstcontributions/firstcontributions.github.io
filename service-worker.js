efed47bbdc7c92130b54a8d8b631c978a23ebbd5
Roshan <roshanjossey@gmail.com>
2018-10-12 01:23:05 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","0003e6bed1dc0c2a225923204310a272"],["manifest.json","f1fbae540a11c453a060b59cf14d5a75"],["static/css/main.63d6905e.css","399c1912e572834c9445d97b267ad310"],["static/js/main.d3e1be0c.js","e19174b3fba090f0d1cc6c5f602003a8"]];
+var precacheConfig = [["index.html","732f4e555e042f76e2777b90c4d16785"],["manifest.json","f1fbae540a11c453a060b59cf14d5a75"],["static/css/main.63d6905e.css","399c1912e572834c9445d97b267ad310"],["static/js/main.2c592097.js","830649fbc66269cc2e3ad0eb492a402c"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
