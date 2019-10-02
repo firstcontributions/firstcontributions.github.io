b2f58b99f606342d9bbb3790621587ec4b025947
Apoorva Dhawan <apoorvadhawan13@gmail.com>
2019-10-02 13:35:34 +0530

    Added Conda Project to the project list

--- src/components/ProjectList/listOfProjects.js
+++ src/components/ProjectList/listOfProjects.js
@@ -534,7 +534,7 @@ const projectList = [
     imageSrc: 'https://conda.io/en/latest/_images/conda_logo.svg',
     githubLink: 'https://github.com/conda',
     description: 'Conda is an open source package management system and environment management system that runs on Windows, macOS and Linux. Conda quickly installs, runs and updates packages and their dependencies. Conda easily creates, saves, loads and switches between environments on your local computer. It was created for Python programs, but it can package and distribute software for any language.',
-    tags: ['ML', 'Python', 'Builds','package manager'],
+    tags: ['ML', 'Python', 'Builds','package manager']
   }
 ];
 export default projectList;
