3ebc544e7e0af6af705d5e11397cdd7a26d1e183
rjossy <rjossy@goodgamestudios.com>
2019-10-09 21:28:38 +0200

    Update react-scripts to latest version

--- src/components/SocialShare/Facebook.jsx
+++ src/components/SocialShare/Facebook.jsx
@@ -4,7 +4,7 @@ import { Icon, IconPaths } from './icons.js';
 const facebookLink = "https://www.facebook.com/sharer/sharer.php?u=https://roshanjossey.github.io/first-contributions&quote=Yay%21%20I%20just%20made%20my%20first%20open%20source%20contribution%20with%20First%20Contributions.%20You%20can%20too,%20by%20following%20a%20simple%20tutorial%20at%20https%3A//goo.gl/66Axwe&hashtag=%23Hacktoberfest"
 
 const FacebookCard = () => (
-    <a className="icon-card twitter" href={facebookLink} target="_blank">
+    <a className="icon-card twitter" href={facebookLink} rel="noopener noreferrer" target="_blank">
         <Icon
             color="rgb(255, 255, 255)"
             size={40}
