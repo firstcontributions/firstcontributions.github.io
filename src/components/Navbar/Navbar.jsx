8769367709b41ec3b95ae53e206d3fef6064ca4a
mohak852 <ajayshri2011@gmail.com>
2020-05-26 02:05:38 +0530

    Added Icons in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './Navbar.css';
-
+import slack from './slack.svg';
+import youtube from './youtube.svg';
+import twitter  from './twitter.svg';
+import github from './github.svg';
 class Navbar extends Component {
     render() {
         return (
             <div className="topnav">
+                <img src={slack} className="slack"/>
                 <a href="https://join.slack.com/t/firstcontributors/shared_invite/enQtNjkxNzQwNzA2MTMwLTVhMWJjNjg2ODRlNWZhNjIzYjgwNDIyZWYwZjhjYTQ4OTBjMWM0MmFhZDUxNzBiYzczMGNiYzcxNjkzZDZlMDM"  target="_blank" rel="noopener noreferrer">Slack</a>
+                <img  src={youtube} className="youtube"/>
                 <a href="https://www.youtube.com/channel/UCMXNFxCvyH5LhUwEcmY8qGQ" target="_blank" rel="noopener noreferrer">YouTube</a>
+                <img   src={twitter} className="twitter"/>
                 <a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer">Twitter</a>
+                <img  src={github} className="github"/>
                 <a href="https://github.com/firstcontributions/first-contributions" target="_blank" rel="noopener noreferrer">GitHub</a>
             </div>
         );
