d729a78706893112f952e50a6156af5bbe9eb011
Tobias Torres <tobias.torres777@gmail.com>
2020-10-22 14:25:39 -0300

    Cambie a componente funcional la clase LinkButton

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './LinkButton.css';
 
-class LinkButton extends Component {
-  render() {
+function LinkButton() {
     return (
       <a className="LinkButton" href="https://github.com/firstcontributions/first-contributions/blob/master/README.md">
         <span> Get started </span>
       </a>
     );
-  }
 }
 
 export default LinkButton;
