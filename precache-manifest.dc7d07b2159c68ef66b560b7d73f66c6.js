9113b8de162e52e0b11f4a7e0242805602668f49
Roshanjossey <roshanjossey@gmail.com>
2020-12-03 13:26:03 +0000

    Deploying to master from  @ 283c834836e7a238f140a4437266d2d1606f1276 🚀

---
+++ precache-manifest.dc7d07b2159c68ef66b560b7d73f66c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb1473aa8c6a102c0a57ba1028f84e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c78d094cc5bc5718277",
+    "url": "/static/css/2.75b42941.chunk.css"
+  },
+  {
+    "revision": "c81da7f6cb62b88ee0b0",
+    "url": "/static/css/main.6ea6a628.chunk.css"
+  },
+  {
+    "revision": "7c78d094cc5bc5718277",
+    "url": "/static/js/2.7df980f6.chunk.js"
+  },
+  {
+    "revision": "c81da7f6cb62b88ee0b0",
+    "url": "/static/js/main.1fc967b4.chunk.js"
+  },
+  {
+    "revision": "72a006878facbd54db25",
+    "url": "/static/js/runtime-main.caf13ca3.js"
+  },
+  {
+    "revision": "0ce4951be5973cbac169171c9d5ac033",
+    "url": "/static/media/github.0ce4951b.svg"
+  },
+  {
+    "revision": "0b5d0ec2f632cbff76c5807fc2694685",
+    "url": "/static/media/slack.0b5d0ec2.svg"
+  },
+  {
+    "revision": "a2cf30e386531c344e2bf3b5290a9418",
+    "url": "/static/media/twitter.a2cf30e3.svg"
+  },
+  {
+    "revision": "4685674478ac384c95c6483af7526079",
+    "url": "/static/media/youtube.46856744.svg"
+  }
+]);
