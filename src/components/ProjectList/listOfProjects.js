a3e8f9e6c76c4f28b21d00802c112ae68683ca87
Alexander Korbonits <akorbonits@assurance.com>
2021-06-24 09:09:33 -0700

    change to contibute page to conform to project list convention

--- src/components/ProjectList/listOfProjects.js
+++ src/components/ProjectList/listOfProjects.js
@@ -83,7 +83,7 @@ const projectList = [
   }, {
     name: 'pandas',
     imageSrc: 'https://camo.githubusercontent.com/981d48e57e23a4907cebc4eb481799b5882595ea978261f22a3e131dcd6ebee6/68747470733a2f2f70616e6461732e7079646174612e6f72672f7374617469632f696d672f70616e6461732e737667',
-    projectLink: 'https://github.com/pandas-dev/pandas',
+    projectLink: 'https://github.com/pandas-dev/pandas/contribute',
     description: 'pandas: powerful Python data analysis toolkit',
     tags: ['Python', 'data science', 'data analysis', 'math', 'Module']
   }, {
