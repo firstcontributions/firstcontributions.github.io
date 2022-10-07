c75e758ea5692b403043389baaa386bfd89c38f6
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2022-10-07 17:56:14 +0530

    � migrating navbar to sidebar

--- src/components/ProjectList/CardsContainer.jsx
+++ src/components/ProjectList/CardsContainer.jsx
@@ -1,152 +1,153 @@
-import React from 'react';
-import Select from 'react-select';
-import each from 'lodash/each'
+import React from "react";
+import Select from "react-select";
+import each from "lodash/each";
 
-import Card from './ProjectsCards';
-import projectList from './listOfProjects';
+import Card from "./ProjectsCards";
+import projectList from "./listOfProjects";
 
-import './css/cards-container.css';
-import './css/search.css';
-import 'react-select/dist/react-select.css';
+import "./css/cards-container.css";
+import "./css/search.css";
+import "react-select/dist/react-select.css";
 
 export default class CardsContainer extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      value: [],
-      filterList: this.sortArrayRandom(projectList)
-    }
+		this.state = {
+			value: [],
+			filterList: this.sortArrayRandom(projectList),
+		};
 
-    this.setTags = new Set();
-    this.filterOptions = [];
-    this.valueList = [];
+		this.setTags = new Set();
+		this.filterOptions = [];
+		this.valueList = [];
 
-    for (let i = 0; i < projectList.length; i++) {
+		for (let i = 0; i < projectList.length; i++) {
+			if (projectList[i].tags) {
+				projectList[i].tags.forEach((tag) => {
+					projectList[i].tags.sort();
+					this.setTags.add(tag.toLowerCase());
+				});
+			}
+		}
 
-      if (projectList[i].tags) {
+		this.setTags.forEach((v) =>
+			this.filterOptions.push({ value: v, label: v })
+		);
+		this.handleSelectChange = this.handleSelectChange.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-        projectList[i].tags.forEach(tag => {
+	handleSelectChange(value) {
+		this.value = value;
+		this.setState({ value });
+		this.handleFilterListUpdate(value);
+	}
 
-          projectList[i].tags.sort()
-          this.setTags.add(tag.toLowerCase())
-        })
-      }
-    }
+	handleFilterListUpdate(value) {
+		let updatedList = [];
 
-    this.setTags.forEach(v => this.filterOptions.push({ value: v, label: v }));
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+		// If no filters
+		if (
+			(!value || value.length === 0) &&
+			(!this.inputValue || this.inputValue.length === 0)
+		) {
+			return this.setState({ filterList: this.sortArrayRandom(projectList) });
+		}
 
-  handleSelectChange(value) {
+		// If tags filter applied
+		if (value && value.length > 0) {
+			const valueList = [];
 
-    this.value = value;
-    this.setState({ value });
-    this.handleFilterListUpdate(value);
-  }
+			value.map((v) => {
+				return valueList.push(v.value);
+			});
 
-  handleFilterListUpdate(value) {
+			each(projectList, (project) => {
+				if (!project.tags) return;
 
-    let updatedList = [];
+				let lowerCaseTags = project.tags.map((v) => v.toLowerCase());
+				if (valueList.every((v) => lowerCaseTags.includes(v))) {
+					updatedList.push(project);
+				}
+			});
+		}
 
-    // If no filters
-    if ((!value || value.length === 0) && (!this.inputValue || this.inputValue.length === 0)) {
-      return this.setState({ filterList: this.sortArrayRandom(projectList) });
-    }
+		// If search input value is not empty
+		if (this.inputValue && this.inputValue.length > 0) {
+			const searchedList = [];
+			each(updatedList.length > 0 ? updatedList : projectList, (project) => {
+				if (
+					project.name.toLowerCase().includes(this.inputValue) ||
+					project.description.toLowerCase().includes(this.inputValue) ||
+					project.tags.toString().toLowerCase().includes(this.inputValue)
+				) {
+					searchedList.push(project);
+				}
+			});
 
-    // If tags filter applied
-    if (value && value.length > 0) {
-      const valueList = [];
+			updatedList = searchedList;
+		}
 
-      value.map(v => {
-        return valueList.push(v.value)
-      });
+		this.setState({ filterList: updatedList });
+	}
 
-      each(projectList, (project) => {
+	// Search input handler
+	handleChange(event) {
+		this.inputValue = event.currentTarget.value;
 
-        if (!project.tags) return;
+		this.inputValue = this.inputValue.trim();
+		this.inputValue = this.inputValue.toLowerCase();
 
-        let lowerCaseTags = project.tags.map(v => v.toLowerCase())
-        if (valueList.every(v => lowerCaseTags.includes(v))) {
+		this.handleFilterListUpdate(this.value);
+	}
 
-          updatedList.push(project);
-        }
-      })
-    }
+	sortArrayRandom(array) {
+		if (Array.isArray(array)) {
+			return array.sort(() => 0.5 - Math.random());
+		}
+		return array;
+	}
 
-    // If search input value is not empty
-    if (this.inputValue && this.inputValue.length > 0) {
-
-      const searchedList = []
-      each(((updatedList.length > 0) ? updatedList : projectList), (project) => {
-
-        if (project.name.toLowerCase().includes(this.inputValue)
-          || project.description.toLowerCase().includes(this.inputValue)
-          || project.tags.toString().toLowerCase().includes(this.inputValue)) {
-
-          searchedList.push(project)
-        }
-      });
-
-      updatedList = searchedList;
-    }
-
-    this.setState({ filterList: updatedList });
-  }
-
-  // Search input handler
-  handleChange(event) {
-
-    this.inputValue = event.currentTarget.value;
-
-    this.inputValue = this.inputValue.trim();
-    this.inputValue = this.inputValue.toLowerCase();
-
-    this.handleFilterListUpdate(this.value)
-  }
-
-  sortArrayRandom(array){
-    if(Array.isArray(array)){
-      return array.sort(()=>0.5-Math.random())
-    }
-    return array
-  }
-
-  render() {
-
-    return (
-      <div>
-        <div id='container'>
-          <div className='inputContainer'>
-            <input id='search' type='text' name='search' placeholder='Search...' onChange={this.handleChange} />
-          </div>
-          <div className='inputContainer'>
-            <Select
-              name='tag-selector'
-              value={this.state.value}
-              onChange={this.handleSelectChange}
-              options={this.filterOptions}
-              multi={true}
-              placeholder='Filter'
-            />
-          </div>
-        </div>
-        <section id='project-list' className='containerLayout'>
-          {this.state.filterList.map((item, key) => {
-            return (
-              <Card
-                key={key}
-                name={item.name}
-                logoLink={item.imageSrc}
-                projectLink={item.projectLink}
-                description={item.description}
-                tags={item.tags}
-              />
-            );
-          })}
-        </section>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div id="container">
+					<div className="inputContainer">
+						<input
+							id="search"
+							type="text"
+							name="search"
+							placeholder="Search..."
+							onChange={this.handleChange}
+						/>
+					</div>
+					<div className="inputContainer">
+						<Select
+							name="tag-selector"
+							value={this.state.value}
+							onChange={this.handleSelectChange}
+							options={this.filterOptions}
+							multi={true}
+							placeholder="Filter"
+						/>
+					</div>
+				</div>
+				<section id="project-list" className="containerLayout">
+					{this.state.filterList.map((item, key) => {
+						return (
+							<Card
+								key={key}
+								name={item.name}
+								logoLink={item.imageSrc}
+								projectLink={item.projectLink}
+								description={item.description}
+								tags={item.tags}
+							/>
+						);
+					})}
+				</section>
+			</div>
+		);
+	}
 }
