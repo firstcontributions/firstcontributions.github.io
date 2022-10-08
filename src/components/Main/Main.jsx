67e13c568d4f9cc8e45094cb00d02fac1f272a55
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2022-10-08 19:37:26 +0530

    adding background image to header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,10 +8,12 @@ import CardsContainer from "../ProjectList/CardsContainer";
 export default function Main() {
 	return (
 		<main className="main">
-			<div className="App-header">
-				<h1>Make Your First Open Source Contribution In 5 Minutes</h1>
+			<div className="header">
+				<div className="App-header">
+					<h1>Make Your First Open Source Contribution In 5 Minutes</h1>
+				</div>
+				<LinkButton />
 			</div>
-			<LinkButton />
 			<CardsContainer />
 		</main>
 	);
