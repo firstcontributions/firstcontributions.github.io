fd8d904f3c8d9d62f3936a31efd13a7bb2d5c9ef
ifeanyihm <iheme.ifeanyi@yahoo.com>
2023-01-25 12:58:31 +0100

    Update CircuitVerse Card Image

--- src/components/ProjectList/listOfProjects.js
+++ src/components/ProjectList/listOfProjects.js
@@ -1182,8 +1182,9 @@ const projectList = [
   },
   {
     name: 'aprenda-go-com-testes',
-    imageSrc: 'https://raw.githubusercontent.com/larien/aprenda-go-com-testes/main/.gitbook/assets/red-green-blue-gophers-smaller.png',
-    projectLink: 'https://github.com/larien/aprenda-go-com-testes',
+    imageSrc: 'https://github.com/larien/aprenda-go-com-testes/blob/main/.gitbook/assets/red-green-blue-gophers-smaller.png',
+    githubLink:
+      'https://github.com/larien/aprenda-go-com-testes',
     description: 'learn easily and quickly',
     tags: ['go']
   },
