2fb4579acb8558c96fe865278b7b28c2d54f2dbf
Palwisha Akhtar <palwishaakhtar@gmail.com>
2023-04-09 21:34:58 -0600

    fix typo

--- src/components/ProjectList/listOfProjects.js
+++ src/components/ProjectList/listOfProjects.js
@@ -1206,7 +1206,7 @@ const projectList = [
     name: 'matplotlib',
     imageSrc: 'https://camo.githubusercontent.com/109927a15915074d15313889468aa9aa688de3b9e38cc4359a01f665d351114e/68747470733a2f2f6d6174706c6f746c69622e6f72672f5f7374617469632f6c6f676f322e737667',
     projectLink: 'https://github.com/matplotlib/matplotlib',
-    description: 'Aa comprehensive library for creating static, animated, and interactive visualizations in Python.',
+    description: 'A comprehensive library for creating static, animated, and interactive visualizations in Python.',
     tags: ['Python, Data Visualization, Plotting']
   },
 ];
