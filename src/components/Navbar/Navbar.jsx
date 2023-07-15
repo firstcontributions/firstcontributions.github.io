7e8cf81a47acd27bb580a1396989a29c04269756
Naresh Nishad <nc.nishad10@gmail.com>
2023-07-15 16:38:33 +0530

    Updated the slack link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import './Navbar.css';
-import slack from './slack.svg';
-import youtube from './youtube.svg';
-import twitter  from './twitter.svg';
-import github from './github.svg';
+import React from "react";
+import "./Navbar.css";
+import slack from "./slack.svg";
+import youtube from "./youtube.svg";
+import twitter from "./twitter.svg";
+import github from "./github.svg";
 
 const Navbar = () => {
   return (
-      <div className="topnav">
-          <a href="https://join.slack.com/t/firstcontributors/shared_invite/zt-1hg51qkgm-Xc7HxhsiPYNN3ofX2_I8FA"  target="_blank" rel="noopener noreferrer">
-              <img src={slack} className="logo" alt="slack logo" />
-              <span>Slack</span>
-          </a>
-          <a href="https://www.youtube.com/channel/UCMXNFxCvyH5LhUwEcmY8qGQ" target="_blank" rel="noopener noreferrer">
-              <img  src={youtube} className="logo" alt="youtube logo" />
-              <span>Youtube</span>    
-          </a>
-          <a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer">
-              <img   src={twitter} className="logo" alt="twitter logo" />
-              <span>Twitter</span>
-          </a>
-          <a href="https://github.com/firstcontributions/first-contributions" target="_blank" rel="noopener noreferrer">
-              <img  src={github} className="logo" alt="github logo" />
-              Github
-          </a>
-      </div>
+    <div className="topnav">
+      <a
+        href="https://join.slack.com/t/firstcontributors/shared_invite/zt-1n4y7xnk0-DnLVTaN6U9xLU79H5Hi62w"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={slack} className="logo" alt="slack logo" />
+        <span>Slack</span>
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UCMXNFxCvyH5LhUwEcmY8qGQ"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={youtube} className="logo" alt="youtube logo" />
+        <span>Youtube</span>
+      </a>
+      <a
+        href="https://twitter.com/1stContribution"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={twitter} className="logo" alt="twitter logo" />
+        <span>Twitter</span>
+      </a>
+      <a
+        href="https://github.com/firstcontributions/first-contributions"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} className="logo" alt="github logo" />
+        Github
+      </a>
+    </div>
   );
 };
 
