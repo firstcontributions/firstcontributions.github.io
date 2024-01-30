945ea9835d2a7c6e39779b8a6f256c517d1e5bf4
2014 <116435491+5atoshiNakamoto@users.noreply.github.com>
2024-01-30 23:55:07 +0200

    Update SocialShare.jsx
    added a period

--- src/components/SocialShare/SocialShare.jsx
+++ src/components/SocialShare/SocialShare.jsx
@@ -6,7 +6,7 @@ import './SocialShare.css';
 
 const SocialShare = () => (
     <section id='social-share'>
-        <h2>Share the news of your first contribution</h2>
+        <h2>Share the news of your first contribution.</h2>
         <div id='social-shares-container'>
             <TwitterCard/>
             <FacebookCard/>
