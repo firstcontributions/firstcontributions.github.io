fae911cebc7c7ff8cf8b993b91b68b85404b9a79
Magdy Demetri <magdymaher129@gmail.com>
2024-02-19 06:09:17 +0200

    this branch wii add responsive style to navbar and to be fixed at top of page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './Navbar.css';
 import slack from './slack.svg';
 import youtube from './youtube.svg';
@@ -6,25 +6,45 @@ import twitter  from './twitter.svg';
 import github from './github.svg';
 
 const Navbar = () => {
+    const [show,setShow]=useState(false);
+    let showClass
+    let mainShow
+    if(show){
+         showClass="topnav show"
+         mainShow="main"
+    }else{
+         showClass="topnav"
+         mainShow=""
+    }
   return (
-      <div className="topnav">
+      <div style={{textAlign:"left"}} className={mainShow}>
+      <button  className="btn"
+      onClick={()=>setShow(!show)}
+      >
+        <div style={{display:"flex",flexDirection:"column",fontSize:"20px",rowGap:"2px",fontWeight:"bolder",lineHeight:"12px",width:"40px",padding:"4px",zIndex:"10",color:"white"}}>
+        <span >---</span>
+        <span>---</span>
+        <span>---</span></div>
+       </button>   
+     <div className={showClass} >
+    
           <a href="https://join.slack.com/t/firstcontributors/shared_invite/zt-1n4y7xnk0-DnLVTaN6U9xLU79H5Hi62w"  target="_blank" rel="noopener noreferrer">
               <img src={slack} className="logo" alt="slack logo" />
               <span>Slack</span>
           </a>
           <a href="https://www.youtube.com/channel/UCMXNFxCvyH5LhUwEcmY8qGQ" target="_blank" rel="noopener noreferrer">
               <img  src={youtube} className="logo" alt="youtube logo" />
-              <span>Youtube</span>    
+              <span className='title'>Youtube</span>    
           </a>
           <a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer">
               <img   src={twitter} className="logo" alt="twitter logo" />
-              <span>Twitter</span>
+              <span className='title'>Twitter</span>
           </a>
           <a href="https://github.com/firstcontributions/first-contributions" target="_blank" rel="noopener noreferrer">
               <img  src={github} className="logo" alt="github logo" />
-              <span>GitHub</span>
+              <span className='title'>GitHub</span>
           </a>
-      </div>
+      </div></div>
   );
 };
 
