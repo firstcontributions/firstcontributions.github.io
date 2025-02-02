f019f0e5575f95cebb5a820d06d264dbba3ab3ae
VaidyaPranav <vaidyapranav987@gmail.com>
2025-02-02 18:54:59 +0530

    Updated footer & Get started button

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import "./LinkButton.css";
 import scrollDown from "./scroll-down.svg";
 
-class LinkButton extends Component {
-  render() {
-    return (
-      <div className="LinkButton-Wrapper">
+/*      <div className="LinkButton-Wrapper">
         <a
           className="LinkButton"
           href="https://github.com/firstcontributions/first-contributions/blob/main/README.md"
@@ -14,6 +11,41 @@ class LinkButton extends Component {
         </a>
         <div className="Scroll-Down-Wrapper">
           <img className="Scroll-Down" onClick={() => { window.scrollBy({ top: window.innerHeight - 80, left: 0, behavior: "smooth" }); }} src={scrollDown} alt="Scroll Down!" width="64" height="64" />
+        </div>
+      </div>
+      
+      <div className="get-started-container">
+        <a href="https://github.com/firstcontributions/first-contributions/blob/main/README.md" className="get-started-btn">
+          Get Started
+        </a>
+      </div>*/
+class LinkButton extends Component {
+  render() {
+    return (
+      <div className="LinkButton-Wrapper">
+        <div className="get-started-container">
+          <a
+            href="https://github.com/firstcontributions/first-contributions/blob/main/README.md"
+            className="get-started-btn"
+          >
+            Get Started
+          </a>
+        </div>
+        <div className="Scroll-Down-Wrapper">
+          <img
+            className="Scroll-Down"
+            onClick={() => {
+              window.scrollBy({
+                top: window.innerHeight - 80,
+                left: 0,
+                behavior: "smooth",
+              });
+            }}
+            src={scrollDown}
+            alt="Scroll Down!"
+            width="64"
+            height="64"
+          />
         </div>
       </div>
     );
