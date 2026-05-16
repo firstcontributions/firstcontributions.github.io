b46fe999a7c6fc8e5eee8d0050d2f4cb02aee09b
Sean Doherty <smaster7772@gmail.com>
2026-05-16 17:14:28 -0500

    fix Servo project link

--- src/data/projects.js
+++ src/data/projects.js
@@ -1315,7 +1315,7 @@ export const projectList = [
   {
     name: "Servo",
     imageSrc: "https://avatars.githubusercontent.com/u/2566135?s=200&v=4",
-    projectLink: "https://github.com/digital-asset/daml/contribute",
+    projectLink: "https://github.com/servo/servo/contribute",
     description:
       "A browser engine designed for applications including embedded use.",
     tags: ["Rust", "Browser", "Servo"],
