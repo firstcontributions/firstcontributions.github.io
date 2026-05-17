e6918228b5c7a6c7179ba966a0b7dfcac24a288b
Sean Doherty <smaster7772@gmail.com>
2026-05-17 02:05:39 -0500

    add Rspack project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1458,4 +1458,12 @@ export const projectList = [
     description: "Ansible is a radically simple IT automation platform that makes your applications and systems easier to deploy.",
     tags: ["Python", "Automation", "Configuration Management"],
   },
+  {
+    name: "Rspack",
+    imageSrc: "https://avatars.githubusercontent.com/u/87694465?s=200&v=4",
+    projectLink: "https://github.com/web-infra-dev/rspack/contribute",
+    description:
+      "Rspack is a fast Rust-based bundler for the web with a modernized webpack-compatible API.",
+    tags: ["Rust", "JavaScript", "TypeScript", "Bundler", "Good First Issue"],
+  },
 ];
