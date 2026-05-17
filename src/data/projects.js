a7b8d17b63c0a57f072c3b12b1a7eb9589edbb05
Sean Doherty <smaster7772@gmail.com>
2026-05-17 02:16:39 -0500

    remove duplicate Open Source Diversity card

--- src/data/projects.js
+++ src/data/projects.js
@@ -722,24 +722,6 @@ export const projectList = [
     ],
   },
   {
-    name: "Open Source Diversity",
-    imageSrc: "https://avatars1.githubusercontent.com/u/31018274?s=200&v=4",
-    projectLink:
-      "https://github.com/opensourcediversity/opensourcediversity.org/contribute",
-    description:
-      "For more diversity & inclusion in free & open source software communities 😊",
-    tags: [
-      "javascript",
-      "html",
-      "css",
-      "diversity",
-      "inclusion",
-      "🎉",
-      "web",
-      "community",
-    ],
-  },
-  {
     name: "Bitcoin",
     imageSrc:
       "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v211-mint-aum-currency-13.jpg?auto=format&bg=F4F4F3&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-1.1.0&mark=rawpixel-watermark.png&markalpha=90&markpad=13&markscale=10&markx=25&q=75&usm=15&vib=3&w=1000&s=435abda621bceebc1362c7e657e06c79",
