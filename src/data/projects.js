1cfdf9576d8190a854eaf8a2138a3109db7c2fc7
Sean Doherty <smaster7772@gmail.com>
2026-05-17 05:03:04 -0500

    add marimo project suggestion

--- src/data/projects.js
+++ src/data/projects.js
@@ -72,6 +72,14 @@ export const projectList = [
     tags: ["Python", "Data Science", "Machine Learning", "ETL", "DAG"],
   },
   {
+    name: "marimo",
+    imageSrc: "https://avatars.githubusercontent.com/u/98563464?v=4",
+    projectLink: "https://github.com/marimo-team/marimo/contribute",
+    description:
+      "marimo is a reactive Python notebook for reproducible experiments and apps.",
+    tags: ["Python", "Notebook", "Data Science", "Reactive"],
+  },
+  {
     name: "altair",
     imageSrc:
       "https://raw.githubusercontent.com/altair-graphql/altair/master/icons/favicon-96x96.png",
